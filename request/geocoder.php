a79c4e0711fce01adb283d3b8ed3dedad5451c91
Vernon de Goede <vernon.de.goede@hva.nl>
2013-04-21 20:14:16 +0200

    Revert "Uitbreidingen"
    This reverts commit 0a27dc6725bb7a2d815bba2d8fe193486fae3bee.

--- request/geocoder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$address = urlencode($_GET['q']);
-$request = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=" . $address . "&sensor=false");
-$json = json_decode($request, true);
-
-?>
